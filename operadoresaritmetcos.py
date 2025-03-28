9ab0e7beacf3503e7bb1bb6a9a5ab8424dbfba00
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 10:00:47 -0300

    primeirocommit

---
+++ operadoresaritmetcos.py
@@ -0,0 +1,34 @@
+# Adição
+numero1 = 10
+numero2 = 20
+
+soma = numero1 + numero2
+print(soma)
+
+# Subtração
+
+subtracao = numero1 - numero2
+print(subtracao)
+
+# Multiplicação
+
+multiplicao = numero1 * numero2
+print(multiplicao)
+
+#Divisão
+
+divisao = numero1 / numero2
+print(divisao)
+
+#Potencia
+
+potencia = numero1 ** numero2
+print(potencia)
+
+#Modulo
+modulo = numero1 % numero2
+print(modulo)
+
+#Resto da divisao
+restodivisao = numero1 // numero2
+print(restodivisao)
