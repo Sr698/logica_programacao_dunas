ed85ef9f0395da35fb6a30fd2a7ebb191679a2b8
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 10:48:05 -0300

    primeirocommit

---
+++ operadorescomparacao.py
@@ -0,0 +1,21 @@
+
+x = 10
+y = 10
+
+# Comparacao de Igualdade
+print(x == y)
+
+# Diferenca
+print(x != y)
+
+# Superioridade 
+print(x > y)
+
+# Inferioridade
+print(x < y)
+
+# Maior ou Igual
+print(x >= y)
+
+# Menor ou Igual
+print(x <= y)
