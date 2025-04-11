e9a06cf533a16c52c40f984daba01db4b3a2983b
FRANCISCO SAMUEL SANTOS REBOUCAS <202503532631@alunos.unifanor.edu.br>
2025-04-11 09:49:32 -0300

    AULA06

---
+++ aula06/estruturasCondidicionais.py
@@ -0,0 +1,11 @@
+
+nota = float(input("Qual é a sua nota: "))
+if nota <= 0 or nota > 10:
+    print("Nota inválida")
+elif nota >= 9:
+    print("Aprovado")
+elif nota >= 7:
+    print("Recuperação")
+else: 
+    print("Reprovado")
+ 
